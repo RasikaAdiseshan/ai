859dbcab7251a45b61cd053b362953d350203d75
RasikaAdiseshan <135004229+RasikaAdiseshan@users.noreply.github.com>
2023-05-30 19:15:19 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,10 @@
+set1={8,0,6,2,4}
+set2={1,2,3,4,5}
+a=set1.union(set2)
+print(a)
+b=set1.intersection(set2)
+print(b)
+c=set1.difference(set2)
+print(c)
+d=set1.symmetric_difference(set2)
+print(d)
